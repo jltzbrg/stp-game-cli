9d4e5f36c5f376c1cd01c1e3ac5be9c52979e224
Julio Litzenberg <julio.litzenberg@gmail.com>
2020-11-03 21:47:41 +0100

    Implement - basic game logic

--- stp_game_cli.dart
+++ stp_game_cli.dart
@@ -1,7 +1,27 @@
 import 'dart:io';
 
+enum Move {
+  stein,
+  schere,
+  papier,
+}
 void main() {
-  stdout.write('Stein, Schere oder Papier? Drücke: (st/sch/pap) ');
-  final input = stdin.readLineSync();
-  print(input);
+  while (true) {
+    stdout.write('Stein, Schere oder Papier? Drücke: (st/sch/pap) ');
+    final input = stdin.readLineSync();
+    if (input == 'st' || input == 'sch' || input == 'pap') {
+      var playerMove;
+      if (input == 'st') {
+        playerMove = Move.stein;
+      } else if (input == 'sch') {
+        playerMove = Move.schere;
+      } else {
+        playerMove = Move.papier;
+      }
+    } else if (input == 'q') {
+      break;
+    } else {
+      print('ungültige Eingabe');
+    }
+  }
 }
